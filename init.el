43016330c551af04e7e136c36d7c3e10aa46f230
ethan <ethan@192>
2024-10-28 16:27:39 +0400

    28/10/24

--- init.el
+++ init.el
@@ -17,25 +17,5 @@
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;g
-;;  '(doom-modeline-bar ((t (:background "gainsboro"))))
-;;  '(doom-modeline-bar-inactive ((t (:background "#585858"))))
-;;  '(hl-line ((t (:extend t :background "gainsboro"))))
-;;  '(warning ((t (:foreground "navajo white" :weight bold)))))
-(put 'narrow-to-page 'disabled nil)
 
-(server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-operandi))
- '(package-selected-packages
-   '(lua-mode which-key use-package sudo-edit ssh smart-compile recompile-on-save rainbow-mode rainbow-delimiters org-superstar org-super-agenda openwith magit lsp-mode go-mode flycheck doom-modeline doct company auctex)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 113 :width normal :foundry "UKWN" :family "Comic Code")))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
